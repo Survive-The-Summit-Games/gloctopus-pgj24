68f035200762c597b985c842e5d8b82a3b4b6978
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2024-05-19 12:00:33 -0400

    Level generation with tilemap

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 
 public class MeshGenerator : MonoBehaviour
 {
@@ -9,265 +10,41 @@ public class MeshGenerator : MonoBehaviour
     public MeshFilter walls;
     public MeshFilter cave;
 
-    public bool is2D;
-
-    List<Vector3> vertices;
-    List<int> triangles;
-
     Dictionary<int, List<Triangle>> triangleDictionary = new Dictionary<int, List<Triangle>>();
     List<List<int>> outlines = new List<List<int>>();
     HashSet<int> checkedVertices = new HashSet<int>();
 
+    Tilemap tilemap;
+    public TileBase[] tilePalette;
+
     public void GenerateMesh(int[,] map, float squareSize)
     {
-
         triangleDictionary.Clear();
         outlines.Clear();
         checkedVertices.Clear();
 
         squareGrid = new SquareGrid(map, squareSize);
 
-        vertices = new List<Vector3>();
-        triangles = new List<int>();
-
-        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+        tilemap = GetComponent<Tilemap>();
+        tilemap.ClearAllTiles();
+        for (int x = 0; x < squareGrid.squares.GetUpperBound(0); x++)
         {
-            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+            for (int y = 0; y < squareGrid.squares.GetUpperBound(1); y++)
             {
-                TriangulateSquare(squareGrid.squares[x, y]);
-            }
-        }
-
-        Mesh mesh = new Mesh();
-        cave.mesh = mesh;
-
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.RecalculateNormals();
-
-        int tileAmount = 10;
-        Vector2[] uvs = new Vector2[vertices.Count];
-        for (int i = 0; i < vertices.Count; i++)
-        {
-            float percentX = Mathf.InverseLerp(-map.GetLength(0) / 2 * squareSize, map.GetLength(0) / 2 * squareSize, vertices[i].x) * tileAmount;
-            float percentY = Mathf.InverseLerp(-map.GetLength(0) / 2 * squareSize, map.GetLength(0) / 2 * squareSize, vertices[i].z) * tileAmount;
-            uvs[i] = new Vector2(percentX, percentY);
-        }
-        mesh.uv = uvs;
-
-
-        if (is2D)
-        {
-            Generate2DColliders();
-        }
-        else
-        {
-            CreateWallMesh();
-        }
-    }
-
-    void CreateWallMesh()
-    {
-
-        CalculateMeshOutlines();
-
-        List<Vector3> wallVertices = new List<Vector3>();
-        List<int> wallTriangles = new List<int>();
-        Mesh wallMesh = new Mesh();
-        float wallHeight = 5;
-
-        foreach (List<int> outline in outlines)
-        {
-            for (int i = 0; i < outline.Count - 1; i++)
-            {
-                int startIndex = wallVertices.Count;
-                wallVertices.Add(vertices[outline[i]]); // left
-                wallVertices.Add(vertices[outline[i + 1]]); // right
-                wallVertices.Add(vertices[outline[i]] - Vector3.up * wallHeight); // bottom left
-                wallVertices.Add(vertices[outline[i + 1]] - Vector3.up * wallHeight); // bottom right
-
-                wallTriangles.Add(startIndex + 0);
-                wallTriangles.Add(startIndex + 2);
-                wallTriangles.Add(startIndex + 3);
-
-                wallTriangles.Add(startIndex + 3);
-                wallTriangles.Add(startIndex + 1);
-                wallTriangles.Add(startIndex + 0);
-            }
-        }
-        wallMesh.vertices = wallVertices.ToArray();
-        wallMesh.triangles = wallTriangles.ToArray();
-        walls.mesh = wallMesh;
-
-        MeshCollider wallCollider = walls.gameObject.AddComponent<MeshCollider>();
-        wallCollider.sharedMesh = wallMesh;
-    }
-
-    void Generate2DColliders()
-    {
-
-        EdgeCollider2D[] currentColliders = gameObject.GetComponents<EdgeCollider2D>();
-        for (int i = 0; i < currentColliders.Length; i++)
-        {
-            Destroy(currentColliders[i]);
-        }
-
-        CalculateMeshOutlines();
-
-        foreach (List<int> outline in outlines)
-        {
-            EdgeCollider2D edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
-            Vector2[] edgePoints = new Vector2[outline.Count];
-
-            for (int i = 0; i < outline.Count; i++)
-            {
-                edgePoints[i] = new Vector2(vertices[outline[i]].x, vertices[outline[i]].z);
-            }
-            edgeCollider.points = edgePoints;
-        }
-
-    }
-
-    void TriangulateSquare(Square square)
-    {
-        switch (square.configuration)
-        {
-            case 0:
-                break;
-
-            // 1 points:
-            case 1:
-                MeshFromPoints(square.centreLeft, square.centreBottom, square.bottomLeft);
-                break;
-            case 2:
-                MeshFromPoints(square.bottomRight, square.centreBottom, square.centreRight);
-                break;
-            case 4:
-                MeshFromPoints(square.topRight, square.centreRight, square.centreTop);
-                break;
-            case 8:
-                MeshFromPoints(square.topLeft, square.centreTop, square.centreLeft);
-                break;
-
-            // 2 points:
-            case 3:
-                MeshFromPoints(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);
-                break;
-            case 6:
-                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.centreBottom);
-                break;
-            case 9:
-                MeshFromPoints(square.topLeft, square.centreTop, square.centreBottom, square.bottomLeft);
-                break;
-            case 12:
-                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreLeft);
-                break;
-            case 5:
-                MeshFromPoints(square.centreTop, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft, square.centreLeft);
-                break;
-            case 10:
-                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.centreBottom, square.centreLeft);
-                break;
-
-            // 3 point:
-            case 7:
-                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.bottomLeft, square.centreLeft);
-                break;
-            case 11:
-                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.bottomLeft);
-                break;
-            case 13:
-                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft);
-                break;
-            case 14:
-                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.centreBottom, square.centreLeft);
-                break;
-
-            // 4 point:
-            case 15:
-                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
-                checkedVertices.Add(square.topLeft.vertexIndex);
-                checkedVertices.Add(square.topRight.vertexIndex);
-                checkedVertices.Add(square.bottomRight.vertexIndex);
-                checkedVertices.Add(square.bottomLeft.vertexIndex);
-                break;
-        }
-
-    }
-
-    void MeshFromPoints(params Node[] points)
-    {
-        AssignVertices(points);
-
-        if (points.Length >= 3)
-            CreateTriangle(points[0], points[1], points[2]);
-        if (points.Length >= 4)
-            CreateTriangle(points[0], points[2], points[3]);
-        if (points.Length >= 5)
-            CreateTriangle(points[0], points[3], points[4]);
-        if (points.Length >= 6)
-            CreateTriangle(points[0], points[4], points[5]);
-
-    }
-
-    void AssignVertices(Node[] points)
-    {
-        for (int i = 0; i < points.Length; i++)
-        {
-            if (points[i].vertexIndex == -1)
-            {
-                points[i].vertexIndex = vertices.Count;
-                vertices.Add(points[i].position);
+                SetTile(x, y);
             }
         }
     }
 
-    void CreateTriangle(Node a, Node b, Node c)
-    {
-        triangles.Add(a.vertexIndex);
-        triangles.Add(b.vertexIndex);
-        triangles.Add(c.vertexIndex);
+    void SetTile(int x, int y) {
+        Square square = squareGrid.squares[x, y];
 
-        Triangle triangle = new Triangle(a.vertexIndex, b.vertexIndex, c.vertexIndex);
-        AddTriangleToDictionary(triangle.vertexIndexA, triangle);
-        AddTriangleToDictionary(triangle.vertexIndexB, triangle);
-        AddTriangleToDictionary(triangle.vertexIndexC, triangle);
-    }
+        if (square.configuration == 0) return;
 
-    void AddTriangleToDictionary(int vertexIndexKey, Triangle triangle)
-    {
-        if (triangleDictionary.ContainsKey(vertexIndexKey))
-        {
-            triangleDictionary[vertexIndexKey].Add(triangle);
-        }
-        else
-        {
-            List<Triangle> triangleList = new List<Triangle>();
-            triangleList.Add(triangle);
-            triangleDictionary.Add(vertexIndexKey, triangleList);
-        }
-    }
 
-    void CalculateMeshOutlines()
-    {
-
-        for (int vertexIndex = 0; vertexIndex < vertices.Count; vertexIndex++)
-        {
-            if (!checkedVertices.Contains(vertexIndex))
-            {
-                int newOutlineVertex = GetConnectedOutlineVertex(vertexIndex);
-                if (newOutlineVertex != -1)
-                {
-                    checkedVertices.Add(vertexIndex);
-
-                    List<int> newOutline = new List<int>();
-                    newOutline.Add(vertexIndex);
-                    outlines.Add(newOutline);
-                    FollowOutline(newOutlineVertex, outlines.Count - 1);
-                    outlines[outlines.Count - 1].Add(vertexIndex);
-                }
-            }
-        }
+        Vector3Int position = new Vector3Int(x, y, 0);
+        TileBase setTile = tilePalette[square.configuration];
+        tilemap.SetTile(position, setTile);
     }
 
     void FollowOutline(int vertexIndex, int outlineIndex)
